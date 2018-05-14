edc36e9305ed8c5d493503c35d951d949a2d672c
Janeth097 <miguel.galvan17@tectijuana.edu.mx>
2018-05-13 22:05:26 -0700

    Inicio de proyecto

---
+++ main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const {app, BrowserWindow} = require("electron");
+const path = require("path");
+const url = require("url");
+
+let window;
+
+function createWindow(){
+  window =  new BrowserWindow({
+    width: 600,
+    height: 800
+  });
+
+  window.loadURL(url.format({
+    pathname: path.join(__dirname, "dist/index.html"),
+    protocol: "file",
+    slashes: true
+  }));
+
+  window.on("closed", () => {
+    window = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (window === null) createWindow();
+});
